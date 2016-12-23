d35febbdf74e6ebd049ac2a2d2b181dcc0f5f77d
hyhoreki <hyh_operation@yeah.net>
2016-12-23 13:24:11 +0800

    Signed-off-by: hyhoreki <hyh_operation@yeah.net>

--- firstone_app/models.py
+++ firstone_app/models.py
@@ -6,8 +6,8 @@ class Passage(models.Model):
 	content = models.CharField(max_length=10000)
 	
 class Questions(models.Model):
-	ask_user=models.IntergerField()
-	answer_user=models.IntergerField()
+	ask_user=models.IntegerField()
+	answer_user=models.IntegerField()
 	question_title=models.CharField(max_length=500)
 	question_text=models.CharField(max_length=3000)
 	question_answer_text=models.CharField(max_length=3000)
