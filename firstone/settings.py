b8075674d744f46c5ecbefc4316bb00d8b45b1b2
hyhoreki <hyh_operation@yeah.net>
2016-12-28 10:41:47 +0800

    1

--- firstone/settings.py
+++ firstone/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'zh_CN'
+LANGUAGE_CODE = 'zh-Hans'
 
 TIME_ZONE = 'UTC'
 
