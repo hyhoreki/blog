b9da5fe1d4ae71e81c79532d375e49c6ad15b953
hyhoreki <hyh_operation@yeah.net>
2016-12-28 14:09:11 +0800

    1

--- firstone_app/views.py
+++ firstone_app/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from django.contrib import auth
 from django.contrib.auth.models import User
 from .models import *
+import json
 
 def index(request):
 	return render(request, 'home.html')
