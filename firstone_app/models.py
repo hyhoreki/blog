abaedbbf979a4b90803f41c75ff7a31da7f18392
hyhoreki <hyh_operation@yeah.net>
2016-12-30 16:41:05 +0800

    1

--- firstone_app/models.py
+++ firstone_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Question(models.Model):
 	qid=models.AutoField(primary_key=True)
-	ask_user_id=models.IntegerField()
+	ask_user_id=models.IntegerField(null=True)
 	question_title=models.CharField(max_length=120)
 	question_describe=models.CharField(max_length=250)
 	question_text=models.TextField()
@@ -12,7 +12,7 @@ class Question(models.Model):
 		
 class Answer(models.Model):
 	aid=models.AutoField(primary_key=True)
-	answer_user_id=models.IntegerField()
+	answer_user_id=models.IntegerField(null=True)
 	answer_question_id=models.IntegerField()
 	answer_text=models.TextField()
 	answer_time=models.DateTimeField()
