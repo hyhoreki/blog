5f739c6521fafb0f0b2f2db9fc2cb642eec9f4b1
hyhoreki <hyh_operation@yeah.net>
2017-01-03 10:09:57 +0800

    1

--- firstone_app/views.py
+++ firstone_app/views.py
@@ -97,7 +97,7 @@ def question_show(request, qid):
 			answer_user=User.objects.get(id=answer.answer_user_id)
 			return render(request, 'question_show.html', {'question_title':question.question_title, 'question_text':question.question_text, 'ask_user_nickname':ask_user.first_name, 'answer_time':answer.answer_time, 'answer_user_nickname':answer_user.first_name})
 		else:
-			return render(request, 'question_show.html', {'question_title':question.question_title, 'question_text':'期待你的答案', 'ask_user_nickname':ask_user.first_name, 'answer_time':answer.answer_time, 'answer_user_nickname':answer_user.first_name})
+			return render(request, 'question_show.html', {'question_title':question.question_title, 'question_text':'期待你的答案', 'ask_user_nickname':ask_user.first_name, 'answer_time':'', 'answer_user_nickname':''})
 	else:
 		return HttpResponseRedirect("/login/")
 		
