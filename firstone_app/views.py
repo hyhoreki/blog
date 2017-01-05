424e4f8f4f30ee966867be7567d6af1d71a90bff
hyhoreki <hyh_operation@yeah.net>
2017-01-05 15:00:23 +0800

    1

--- firstone_app/views.py
+++ firstone_app/views.py
@@ -100,7 +100,7 @@ def question_show(request, qid):
 		if Answer.objects.all():
 			answer= Answer.objects.filter(answer_question_id=qid).order_by('?')[0]
 			answer_user=User.objects.get(id=answer.answer_user_id)
-			return render(request, 'question_show.html', {'question_title':question.question_title, 'question_text':question.question_text, 'ask_user_nickname':ask_user.first_name, 'answer_text':answer.answer_text, 'answer_time':answer.answer_time, 'answer_user_nickname':answer_user.first_name, 'answer_aid':answer.answer_aid})
+			return render(request, 'question_show.html', {'question_title':question.question_title, 'question_text':question.question_text, 'ask_user_nickname':ask_user.first_name, 'answer_text':answer.answer_text, 'answer_time':answer.answer_time, 'answer_user_nickname':answer_user.first_name, 'answer_aid':answer.aid})
 		else:
 			return render(request, 'question_show.html', {'question_title':question.question_title, 'question_text':question.question_text, 'ask_user_nickname':ask_user.first_name, 'answer_user_nickname':''})
 	else:
