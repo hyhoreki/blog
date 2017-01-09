daee4c215aada14204f99ab584e7526ed1178dde
hyhoreki <hyh_operation@yeah.net>
2017-01-09 18:04:51 +0800

    修改关注功能

--- firstone_app/views.py
+++ firstone_app/views.py
@@ -131,7 +131,9 @@ def attention_question(request, qid, action):
 	if request.user is not None and request.user.is_active:
 		if action == '0':
 			Attention_question.objects.filter(question_id=qid, attention_user_id=request.user.id).delete()
+			attention_dict={'state':'0'}
 		elif action == '1':
 			Attention_question.objects.create(question_id=qid, attention_user_id=request.user.id)
-		url="/question/"+qid
-		return HttpResponseRedirect(url)
+			attention_dict={'state':'1'}
+		attention_dict=serializers.serialize("json", attention_dict)
+		return HttpResponse(attention_dict, content_type='application/json')
